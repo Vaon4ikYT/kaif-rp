2d6c2a27b35f5a274faee58c7997d01f0b2f3c1b
Vaon4ikYT <123131312+Vaon4ikYT@users.noreply.github.com>
2023-01-20 03:30:19 +0300

    Add files via upload

---
+++ donate.php
@@ -0,0 +1,85 @@
+
+ 
+
+<head>
+	<meta charset="utf-8">
+	<meta content="width=device-width, initial-scale=1" name="viewport">
+	<link href="/classes/css/news.css" rel="stylesheet">
+	<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+	<script type='text/javascript' src='http://samp-stats.ru/web/api-12173.js'></script>
+	<title>Россия Дрифт</title>
+</head>
+<body>
+		﻿		                		<div class="container head">
+			<div class="col-md-4 wow fadeInLeft" data-wow-delay="1s">
+				<h3>Личный кабинет</h3><br>
+				<a class="hlink hl-left" href="/page/userpanel/">Перейти в личный кабинет</a>
+			</div>
+			<div class="col-md-4 wow fadeInDown"><img src="/classes/img/logo.png"></div>
+			<div class="col-md-4 wow fadeInRight" data-wow-delay="1s">
+				<h3>Играть на сервере</h3><br>
+				<a class="hlink hl-right" href="samp://83.222.105.54:8358">Подключиться к серверу</a><br><br><br>
+			</div>
+		</div>
+		<div class="container nav wow fadeInDown" data-wow-delay="1s">
+			<a href="/index.php">Главная</a>
+			<a href="https://vk.com/rd.rdmpru">Форум</a>
+			<a href="/donate.php">Донат</a>
+			<a href="/page/userpanel/">Личный кабинет</a>
+		</div>		<div class="container">
+			<div class="col-md-12 post">
+				<h1>
+					Донат<span class="date"></span>
+				</h1>
+				<p>
+					<html>
+<head>
+<meta http-equiv="Content-type" content="text/html; charset=windows-1251" />
+<title>Донат</title>
+</head>
+
+<body>
+
+
+
+
+<form id=pay name=pay method="POST" action="http://dj-sergey.ucoz.net/send.php">
+<hr>
+<p><h3>Приём платежей через WebMoney</h3></p>
+Курс 1 рубль = 1 Gold Coins
+<p>
+	<h4>Введите сумму</h4>
+	<input type="text" name="LMI_PAYMENT_AMOUNT" value="10">
+	<input type="hidden" name="LMI_PAYMENT_DESC" value="">
+	<h4>Введите Ваш игровой ник</h4>
+	<input type="text" name="Name" value="">
+	<input type="hidden" name="LMI_PAYMENT_NO" value="">
+	<input type="hidden" name="LMI_PAYEE_PURSE" value="R231880406743">
+	<input type="hidden" name="LMI_SIM_MODE" value="0">
+	<input type="hidden" name="LMI_SHOP_ID" value="111">
+</p>
+<p>	
+
+	<input type="submit" value="Оплатить">
+</p>
+<hr>
+
+</form>
+
+</body>
+</html>				</p>
+			</div>
+		</div>
+		
+		<div class="container pagination">
+		</div>
+
+		<div class="footer">
+			﻿		<div class="wow fadeIn" data-wow-delay="1s">
+			<img src="/classes/img/logo_flat.png"><br>
+			<a href="/index.php">Главная</a>
+			<a href="http://dj-sergey.ucoz.net/">Форум</a>
+			<a href="/donate.php">Донат</a>
+			<a href="/page/userpanel">Личный кабинет</a>
+			<p>bicbai.ru © 2016 г.</p>
+		</div>		</div>
